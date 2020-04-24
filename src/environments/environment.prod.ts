cd83914ba10e8c2fc250e35178f58d4322c0778b
Prashant Rizal <rizalprashant@Prashants-MacBook-Pro.local>
2020-04-24 16:17:07 -0500

    Firebase env setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDNVGXas8jZJ_lZ-uINbTFXOCHXxJF6Ugw",
+    authDomain: "rizalkitchen-29d05.firebaseapp.com",
+    databaseURL: "https://rizalkitchen-29d05.firebaseio.com",
+    projectId: "rizalkitchen-29d05",
+    storageBucket: "rizalkitchen-29d05.appspot.com",
+    messagingSenderId: "875483626516",
+    appId: "1:875483626516:web:a7cdc5e0fe8f6567cffb01",
+    measurementId: "G-MZKMWT2NVB"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDNVGXas8jZJ_lZ-uINbTFXOCHXxJF6Ugw",
+    authDomain: "rizalkitchen-29d05.firebaseapp.com",
+    databaseURL: "https://rizalkitchen-29d05.firebaseio.com",
+    projectId: "rizalkitchen-29d05",
+    storageBucket: "rizalkitchen-29d05.appspot.com",
+    messagingSenderId: "875483626516",
+    appId: "1:875483626516:web:a7cdc5e0fe8f6567cffb01",
+    measurementId: "G-MZKMWT2NVB"
+  }
 };
 
 /*
