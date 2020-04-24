18b860bbce5db8ac5c4fe8d34547562bde1004ed
Prashant Rizal <rizalprashant@Prashants-MacBook-Pro.local>
2020-04-24 18:09:52 -0500

    Added routes and components required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,16 @@ import {AngularFireModule} from 'angularfire2';
 import { environment } from 'src/environments/environment';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import {RouterModule} from '@angular/router';
+import { componentFactoryName } from '@angular/compiler';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
 
 //todo ipmport angularfire2database
 
@@ -15,12 +25,30 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     BsNavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent},
+      {path:'products', component: ProductsComponent},
+      {path:'shopping-cart', component: ShoppingCartComponent},
+      {path:'check-out', component: CheckOutComponent},
+      {path:'order-success', component: OrderSuccessComponent},
+      {path:'login', component: LoginComponent},
+      {path:'admin/products', component: AdminProductsComponent},
+      {path:'admin/orders', component: AdminOrdersComponent},
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
