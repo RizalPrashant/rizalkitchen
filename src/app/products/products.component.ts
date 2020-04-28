5cf52455e6dbf3b2e824dc27fd834cbbd706e3fb
Prashant Rizal <rizalprashant@Prashants-MacBook-Pro.local>
2020-04-28 17:42:25 -0500

    Product page layout setup

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  product$;
+  categories$;
 
-  constructor() { }
+  constructor(private productService: ProductService, private categoryService: CategoryService) { 
+    this.product$ = this.productService.getAll().snapshotChanges()
+    .pipe(map(items => {
+      return items.map(a => {
+        const key = a.payload.key;
+        const val = a.payload.val();
+        return {key, val};
+      })
+    }))
 
-  ngOnInit(): void {
+    this.categories$ = categoryService.getCategories();
   }
-
 }
